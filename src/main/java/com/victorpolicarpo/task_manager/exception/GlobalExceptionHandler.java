70328e428bbf59586223d1e4dbfc2ae80ec6be29
Victor-Policarpo <victorpolicarposilva@gmail.com>
2026-09-03 14:01:41 -0300

    remove: remove conflicting custom MethodArgumentNotValidException class

--- src/main/java/com/victorpolicarpo/task_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/victorpolicarpo/task_manager/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.victorpolicarpo.task_manager.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
